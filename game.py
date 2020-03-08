087b385ebcf68ece7193aad0c4a7291789a274a0
Miguel Rafael Mateo <climaxfran@gmail.com>
2020-03-07 20:16:28 -0400

    testing game

--- game.py
+++ game.py
@@ -7,7 +7,7 @@ crup = Crupier()
 dice =  Dice()
 dice.status()
 import os
-po = Player()
+player = Player()
 card = Deck_of_cards()
 
 #This function is responsible for creating the players' keys and assigning them, a name, their initial letters and some cards
@@ -25,32 +25,43 @@ def generate_players():
                 name_of_player = input("Entry name of player"+str(delimiter+1)+" ➤ ")
                 os.system("clear")
                 print("Select you icon")
+                
+                def select():
 
-                number_icon = 1
-                for icons in player.icono_for_player:
-                    print(number_icon,icons)
-                    number_icon += 1
-                select_icon = input("insert a number of you icon ➤ ")
-                os.system("clear")
-                if select_icon.isdigit():
-                    if int(select_icon) > 0 and int(select_icon) <= 5:
-                        player.players.update({
-                            'player'+str(delimiter+1):{ 
-                            'name':  name_of_player,
-                            'icon':  player.icono_for_player[int(select_icon)-1],
-                            'state': True,
-                            'chip': 10000,
-                            'point': 0,
-                            'cards': crup.Player_curret_hand}})
+                    number_icon = 1
 
-                        crup.Player_curret_hand  = []
+                    for icons in player.icono_for_player:
+                        print(number_icon,icons)
+                        number_icon += 1
+
+                    select_icon = input("insert a number of you icon ➤ ")
+                    os.system("clear")
+
+                    if select_icon.isdigit():
+
+                        if int(select_icon) > 0 and int(select_icon) <= 5:
+
+                            player.players.update({
+                                'player'+str(delimiter+1):{ 
+                                'name':  name_of_player,
+                                'icon':  player.icono_for_player[int(select_icon)-1],
+                                'state': True,
+                                'chip': 10000,
+                                'point': 0,
+                                'cards': crup.Player_curret_hand}})
+
+                            crup.Player_curret_hand  = []
+
+                        else:
+                            os.system("clear")
+                            input("error the option you have inserted is not valid test with a number from 1 to 5 press enter for continue") 
+                            select()
                     else:
-                        os.system("clear")
-                        input("error the option you have inserted is not valid test with a number from 1 to 5 press enter for continue") 
-                        generate_players()  
-                else:
-                     input("error the option you have inserted is not valid test with a number from 1 to 5 press enter for continue") 
-                     generate_players()
+                            input("error the option you have inserted is not valid test with a number from 1 to 5 press enter for continue") 
+                            select()
+
+                select()
+
         else:
             os.system("clear")
             input("Error entering the number of players, try a number from 1 to 4 enter to continue... ")
@@ -75,7 +86,7 @@ def system_of_turns():
                 moviment = input("1) stand  2) ask for letters  3) backing out: ➤ ")
 
                 if moviment.isdigit():
-                    if int(moviment) <= 3:
+                    if int(moviment) <= 1:
                         if int(moviment) == 1:
                             os.system("clear")
                             print(player.players['player'+str(delimiter+1)])
