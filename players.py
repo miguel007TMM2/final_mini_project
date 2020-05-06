6ac2fc092d7e48452c2fbd3cb3f708213b6462e2
Cryp010 <ichigooncross@gmail.com>
2020-05-06 15:02:35 -0400

    refact funcion players

--- players.py
+++ players.py
@@ -18,24 +18,24 @@ class Player:
         self.bet_state = bet_state
 
 
-    def calculate_player_point(self, iterator, player):
+    def calculate_player_point(self):
 
         point = 0
-        for value_card in player[iterator].cards:
+        for value_card in self.cards:
             point +=  value_card.value
 
         return point 
 
-    def reset_player_data(self, iterator, player, two_cards):
+    def reset_player_data(self):
 
-        if player[iterator].state == True:
+        if self.state == True:
 
-            player[iterator].cards = ''
-            player[iterator].point = ""
-            player[iterator].bet = 0
-            player[iterator].bet_state = False
-            player[iterator].cards = two_cards
-            player[iterator].point = self.calculate_player_point(iterator, player)
+            self.cards = ''
+            self.point = ""
+            self.bet = 0
+            self.bet_state = False
+            self.cards = []
+            self.point = 0
         
         
 
